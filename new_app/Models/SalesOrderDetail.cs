a82b7ca628922aa7121762e7df254c0161a16dc7
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-10-02 10:43:11 +0200

    QA: Refactor SalesOrderDetail model to .NET 8 standards with file-scoped namespace and virtual navigation properties

--- new_app/Models/SalesOrderDetail.cs
+++ new_app/Models/SalesOrderDetail.cs
@@ -8,7 +8,12 @@ public partial class SalesOrderDetail
     public int ProductId { get; set; }
     public decimal UnitPrice { get; set; }
     public decimal UnitPriceDiscount { get; set; }
+    
+    /// <summary>
+    /// Computed column configured in DbContext
+    /// </summary>
     public decimal LineTotal { get; set; }
+    
     public Guid Rowguid { get; set; }
     public DateTime ModifiedDate { get; set; }
 
